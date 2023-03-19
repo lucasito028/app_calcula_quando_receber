152a895f0786945534018f9134ba64ed0b4bb945
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2023-03-19 10:41:25 -0300

    alguns alteração

--- js/calcula.js
+++ js/calcula.js
@@ -1,5 +1,33 @@
 function mostrar(){
     var data = document.getElementById("data").value;
-    data = new Date();
+    data = new Date(data);
     document.getElementById("demo").innerHTML = data.getDate();
+}
+
+function parcelado(){
+    
+    //Valores coletados
+    var data = document.getElementById("data").value;
+
+    //Valores transformados
+    var parc = 4;
+    var valor = 120
+    var d = 25;
+    let i = 0;
+
+    valor = valor / parc;
+
+    d = d + 2;
+
+    while(i < parc){
+    d = d + 30;
+   
+   const date = new Date(2017, 5, d);
+   
+   console.log(date.toLocaleDateString());
+   
+   console.log(valor);
+   
+   i++;
+}
 }
